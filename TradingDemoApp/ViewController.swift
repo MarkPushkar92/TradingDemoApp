d5c57c483dcc711ee15e8889585a7725ef114c5f
Mark Pushkar <mark.pushkar92@gmail.com>
2023-05-24 14:21:42 +0700

    Initial Commit

---
+++ TradingDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TradingDemoApp
+//
+//  Created by Марк Пушкарь on 24.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
